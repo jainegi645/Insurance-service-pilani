f52387335ec92e673b60fa356d1ecd7b2cb5f0cd
jai negi <jainegi645@gmail.com>
2021-12-28 20:08:39 +0530

    added email funtionlality

--- components/ContactMap.js
+++ components/ContactMap.js
@@ -92,7 +92,7 @@ function ContactMap() {
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
               ></textarea>
             </div>
-            <a href = "mailto:jainegi645@gmail.com" target='_blank' className="inline-block text-white text-center bg-yellow-500 border-0 py-2 px-6 focus:outline-none  rounded text-lg">
+            <a href = {"mailto: insuranceservicepilani@gmail.com?Subject=Query%20regarding%20insurance%20sservices&body=" + message} target='_blank' className="inline-block text-white text-center bg-yellow-500 border-0 py-2 px-6 focus:outline-none  rounded text-lg">
               Submit
             </a>
           </div>
